50f23ce114ac07f574505af0ab706e8ce42aae9e
Magnus Egelberg <magnus@egelberg.se>
2014-09-23 16:09:34 +0200

    -

---
+++ index.js
@@ -0,0 +1,56 @@
+var express = require('express')
+var app = express();
+
+/*
+
+url: postgres://zmqeszuvhwmooa:ov0vcsWLVsLQczUE4JdiwymI9N@ec2-54-225-156-230.compute-1.amazonaws.com:5432/ddidqfrm0e850n
+host: ec2-54-225-156-230.compute-1.amazonaws.com
+database: ddidqfrm0e850n
+user: zmqeszuvhwmooa
+port: 5432
+password: ov0vcsWLVsLQczUE4JdiwymI9N
+*/
+
+
+app.set('port', (process.env.PORT || 8000))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(request, response) {
+  response.json({message:'Hello World!' , postgres:process.env.DATABASE_URL, port:process.env.PORT});
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
+
+
+var Pusher = require('pusher');
+
+var pusher = new Pusher({
+  appId: '90278',
+  key: 'bb47773b315bd7352c6b',
+  secret: '80e94870d0ed7c51a967'
+/*
+  encrypted: ENCRYPTED, // optional, defaults to false
+  host: 'HOST', // optional, defaults to api.pusherapp.com
+  port: PORT, // optional, defaults to 80 for unencrypted and 443 for encrypted
+*/
+
+});
+
+var pg = require('pg');
+
+app.get('/db', function (request, response) {
+
+//pusher.trigger('test_channel', 'my_event', { message: "hello world" });
+
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.send(result.rows); }
+    });
+  });
+})
