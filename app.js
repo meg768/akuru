c8973973c917a09f19709228a84a9f9d707836e7
meg768 <magnus@egelberg.se>
2014-12-31 01:07:43 +0100

    -

--- app.js
+++ app.js
@@ -245,8 +245,10 @@ function main() {
 		var displayTime = rand(0, 59);
 		
 		var rule = new schedule.RecurrenceRule();		
-		rule.minute = [displayTime, (displayTime + 30) % 60];
-		rule.hour = [7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
+		//rule.minute = [displayTime, (displayTime + 30) % 60];
+		//rule.hour = [7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
+
+		rule.minute = [displayTime, (displayTime + 30) % 60, (displayTime + 15) % 60, (displayTime + 45) % 60];
 		
 		schedule.scheduleJob(rule, function() {
 	
