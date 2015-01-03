780ffd5284274ae1e1983639189083bbb2248eac
meg768 <magnus@egelberg.se>
2015-01-03 22:09:29 +0100

    -

--- app.js
+++ app.js
@@ -385,6 +385,24 @@ function main() {
 		});		
 
 
+		// Change defaukt animation every hour
+		rule = new schedule.RecurrenceRule();
+		rule.hour = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
+		rule.minute = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55];
+
+		schedule.scheduleJob(rule, function() {
+			
+			sendMessage('settings', {
+				defaultAnimation: {
+					command:Math.random() < 0.5 ? './run-clock' : './run-rain',
+					args: ['-d', -1]
+					
+				}
+			});
+
+		});		
+
+
 	}
 
 	function schedulePing() {
