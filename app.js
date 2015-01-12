a01881cff3358a786f720fa309f3bdf3972bd009
meg768 <magnus@egelberg.se>
2015-01-12 21:11:04 +0100

    -

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var http = require("http");
+var sprintf = require('./sprintf');
 var socketIO, server;
 
 function main() {
@@ -25,53 +26,6 @@ function main() {
 		return items[Math.floor((Math.random() * items.length))];
 	}
 	
-	function sprintf() {
-	
-		function str_repeat(i, m) {
-			for (var o = []; m > 0; o[--m] = i);
-			return o.join('');
-		}
-	
-		var i = 0, a, f = arguments[i++], o = [], m, p, c, x, s = '';
-		while (f) {
-			if (m = /^[^\x25]+/.exec(f)) {
-				o.push(m[0]);
-			}
-			else if (m = /^\x25{2}/.exec(f)) {
-				o.push('%');
-			}
-			else if (m = /^\x25(?:(\d+)\$)?(\+)?(0|'[^$])?(-)?(\d+)?(?:\.(\d+))?([b-fosuxX])/.exec(f)) {
-				if (((a = arguments[m[1] || i++]) == null) || (a == undefined)) {
-					throw('Too few arguments.');
-				}
-				if (/[^s]/.test(m[7]) && (typeof(a) != 'number')) {
-					throw('Expecting number but found ' + typeof(a));
-				}
-				switch (m[7]) {
-					case 'b': a = a.toString(2); break;
-					case 'c': a = String.fromCharCode(a); break;
-					case 'd': a = parseInt(a); break;
-					case 'e': a = m[6] ? a.toExponential(m[6]) : a.toExponential(); break;
-					case 'f': a = m[6] ? parseFloat(a).toFixed(m[6]) : parseFloat(a); break;
-					case 'o': a = a.toString(8); break;
-					case 's': a = ((a = String(a)) && m[6] ? a.substring(0, m[6]) : a); break;
-					case 'u': a = Math.abs(a); break;
-					case 'x': a = a.toString(16); break;
-					case 'X': a = a.toString(16).toUpperCase(); break;
-				}
-				a = (/[def]/.test(m[7]) && m[2] && a >= 0 ? '+'+ a : a);
-				c = m[3] ? m[3] == '0' ? '0' : m[3].charAt(1) : ' ';
-				x = m[5] - String(a).length - s.length;
-				p = m[5] ? str_repeat(c, x) : '';
-				o.push(s + (m[4] ? a + p : p + a));
-			}
-			else {
-				throw('Huh ?!');
-			}
-			f = f.substring(m[0].length);
-		}
-		return o.join('');
-	}
 
 
 	function addCmd(cmd) {
@@ -430,6 +384,100 @@ function main() {
 			ping();	
 		});
 	}
+	
+	
+
+	function enableStockQuotes() {
+		var rule, schedule = require('node-schedule');
+	
+	
+		function display(text) {
+			var packet = {};
+			packet.message = text;
+			packet.textcolor = "blue";
+			sendMessage('text', packet);	
+			
+		}
+		function getStockQuotes(symbols, callback) {
+			
+			var request = require('request');
+			var util = require('util');
+			var tickers = '';
+			
+			for (var index in symbols) {
+				if (tickers != '')
+					tickers += ', ';
+					
+				tickers += '"' + symbols[index] + '"';
+			}
+		
+			var url = sprintf('https://query.yahooapis.com/v1/public/yql?q=%s&format=%s&env=%s&callback=%s', encodeURIComponent(sprintf('select * from yahoo.finance.quote where symbol in (%s)', tickers)), 'json', encodeURIComponent('store://datatables.org/alltableswithkeys'), '');
+		
+			request(url, function (error, response, body) {
+				try {
+					if (error)
+						throw error;
+						
+					if (response.statusCode == 200) {
+						var json = JSON.parse(body);
+						var quotes = json.query.results.quote;
+						
+						if (!util.isArray(quotes))
+							quotes = [quotes];
+		
+						// Convert to numbers
+						for (var index in quotes) {
+							var quote = quotes[index];
+							console.log(quote);
+							
+							quote.Change = parseFloat(quote.Change);
+							quote.AverageDailyVolume = parseFloat(quote.AverageDailyVolume);
+							quote.Volume = parseFloat(quote.Volume);
+							quote.LastTradePriceOnly = parseFloat(quote.LastTradePriceOnly);
+						}
+					
+							
+						callback(quotes);				
+					}
+					else
+						throw new Error('Invalid status code');
+				}
+				catch(error) {
+					console.log(error);
+						
+				}
+				
+			});
+		}
+	
+	
+		rule = new schedule.RecurrenceRule();
+		rule.minute = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 3, 34, 35, 36, 37, 38, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59];
+	//	rule.hour = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22];
+		
+		schedule.scheduleJob(rule, function() {
+			getStockQuotes(['AAPL', 'PHI.ST', 'HM-B.ST', 'ARCC', 'NCC-B.ST', 'INDU-C.ST', 'SHB-B.ST'], function(quotes) {
+			
+				
+				for (var index in quotes) {
+					var quote = quotes[index];
+					var percentPris = (((quote.LastTradePriceOnly - quote.Change) / quote.LastTradePriceOnly) - 1) * 100;
+					var percentOmsattning = 100 * ((quote.Volume / quote.AverageDailyVolume) - 1);
+					var text = '';
+					
+					text += quote.symbol + ' ';;
+					text += percentPris > 0 ? sprintf('+%.2f%% ', percentPris) : sprintf('%.2f%% ', percentPris);
+					text += percentOmsattning > 0 ? sprintf('+%.0f%%', percentOmsattning) : sprintf('%.0f%%', percentOmsattning);
+					
+					display(text);
+				}
+			
+				
+			});
+		});	
+		
+	}
+	
 
 	function startServer() {
 		var express = require("express");
---
+++ test.js
@@ -0,0 +1,105 @@
+
+
+	var app = require('express')();
+	var sprintf = require('./sprintf');
+
+	
+	app.listen(3000, function() {
+		console.log("Node app is running at localhost:" + 3000);
+	});	
+	
+
+
+
+
+
+
+function enableStockQuotes() {
+	var rule, schedule = require('node-schedule');
+
+
+	function display(text) {
+		console.log(text);
+		
+	}
+	function getStockQuotes(symbols, callback) {
+		
+		var request = require('request');
+		var util = require('util');
+		var tickers = '';
+		
+		for (var index in symbols) {
+			if (tickers != '')
+				tickers += ', ';
+				
+			tickers += '"' + symbols[index] + '"';
+		}
+	
+		var url = sprintf('https://query.yahooapis.com/v1/public/yql?q=%s&format=%s&env=%s&callback=%s', encodeURIComponent(sprintf('select * from yahoo.finance.quote where symbol in (%s)', tickers)), 'json', encodeURIComponent('store://datatables.org/alltableswithkeys'), '');
+	
+		request(url, function (error, response, body) {
+			try {
+				if (error)
+					throw error;
+					
+				if (response.statusCode == 200) {
+					var json = JSON.parse(body);
+					var quotes = json.query.results.quote;
+					
+					if (!util.isArray(quotes))
+						quotes = [quotes];
+	
+					// Convert to numbers
+					for (var index in quotes) {
+						var quote = quotes[index];
+						console.log(quote);
+						
+						quote.Change = parseFloat(quote.Change);
+						quote.AverageDailyVolume = parseFloat(quote.AverageDailyVolume);
+						quote.Volume = parseFloat(quote.Volume);
+						quote.LastTradePriceOnly = parseFloat(quote.LastTradePriceOnly);
+					}
+				
+						
+					callback(quotes);				
+				}
+				else
+					throw new Error('Invalid status code');
+			}
+			catch(error) {
+				console.log(error);
+					
+			}
+			
+		});
+	}
+
+
+	rule = new schedule.RecurrenceRule();
+	rule.minute = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 3, 34, 35, 36, 37, 38, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59];
+//	rule.hour = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22];
+	
+	schedule.scheduleJob(rule, function() {
+		getStockQuotes(['AAPL', 'PHI.ST', 'HM-B.ST', 'ARCC', 'NCC-B.ST', 'INDU-C.ST', 'SHB-B.ST'], function(quotes) {
+		
+			
+			for (var index in quotes) {
+				var quote = quotes[index];
+				var percentPris = (((quote.LastTradePriceOnly - quote.Change) / quote.LastTradePriceOnly) - 1) * 100;
+				var percentOmsattning = 100 * ((quote.Volume / quote.AverageDailyVolume) - 1);
+				var text = '';
+				
+				text += quote.symbol + ' ';;
+				text += percentPris > 0 ? sprintf('+%.2f%% ', percentPris) : sprintf('%.2f%% ', percentPris);
+				text += percentOmsattning > 0 ? sprintf('+%.0f%%', percentOmsattning) : sprintf('%.0f%%', percentOmsattning);
+				
+				display(text);
+			}
+		
+			
+		});
+	});	
+	
+}
+
+enableStockQuotes();
