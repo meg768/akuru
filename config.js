e7679679bea6871f3815f8f283dda6417b89cc2a
meg768 <magnus@egelberg.se>
2015-01-13 18:41:07 +0100

    -

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 
 
-module.exports = {};
+module.exports = {
 
 	'timezone' : 'Europe/Stockholm',
 	'stocks' : ['PFE', 'PHI.ST', 'HM-B.ST', 'ARCC', 'NCC-B.ST', 'INDU-C.ST', 'SHB-B.ST', 'COS.TO', 'CAST.ST']
