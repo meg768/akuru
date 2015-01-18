2cf9b871ee360ac92b5e6dfcb5f8beb84741ce26
meg768 <magnus@egelberg.se>
2015-01-18 17:20:27 +0100

    -

--- app.js
+++ app.js
@@ -114,7 +114,7 @@ function enableRSS() {
 	var RSS = require('./rss');
 	var rss = new RSS();
 
-	rss.subscribe('SvD', 'http://www.svd.se/?service=rss&type=latest');
+	rss.subscribe('SvD', 'http://www.svd.se/?service=rss&type=senastenytt');
 	rss.subscribe('SDS', 'http://www.sydsvenskan.se/rss.xml');
 	rss.subscribe('Di', 'http://www.di.se/rss');
 	rss.subscribe('Google', 'http://news.google.com/news?pz=1&cf=all&ned=sv_se&hl=sv&topic=h&num=3&output=rss');
